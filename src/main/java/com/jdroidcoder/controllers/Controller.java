34af9bd373be3a8c30dd1616f43845964c795571
anonunfeeling <anonunfeeling1@gmail.com>
2017-01-21 15:16:38 +0200

    add test method and screenshots

--- src/main/java/com/jdroidcoder/controllers/Controller.java
+++ src/main/java/com/jdroidcoder/controllers/Controller.java
@@ -3,18 +3,20 @@ package com.jdroidcoder.controllers;
 import com.jdroidcoder.dto.BatchPointsDto;
 import com.jdroidcoder.dto.PointDto;
 import com.jdroidcoder.services.InfluxDBService;
+import org.influxdb.InfluxDB;
 import org.influxdb.dto.BatchPoints;
+import org.influxdb.dto.Point;
 import org.influxdb.dto.Query;
 import org.influxdb.dto.QueryResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by jdroidcoder on 20.01.17.
@@ -60,5 +62,35 @@ public class Controller {
     @GetMapping("/query/{dbName}")
     public ResponseEntity<List<List<QueryResult.Result>>> query(@PathVariable String dbName) {
         return ResponseEntity.ok(Arrays.asList(influxDBService.sendQuery(new Query("SELECT * FROM cpu", dbName)).getResults()));
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<List<List<QueryResult.Result>>> test() {
+        String dbName = "test1";
+        influxDBService.createDataBase(dbName);
+
+        BatchPoints batchPoints = BatchPoints
+                .database(dbName)
+                .tag("async", "true")
+                .retentionPolicy("autogen")
+                .consistency(InfluxDB.ConsistencyLevel.ALL)
+                .build();
+        Point point1 = Point.measurement("cpu")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .addField("idle", 111111L)
+                .addField("user", 9L)
+                .addField("system", 1L)
+                .build();
+        Point point2 = Point.measurement("disk")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .addField("used", 80L)
+                .addField("free", 1L)
+                .build();
+        batchPoints.point(point1);
+        batchPoints.point(point2);
+        influxDBService.writeToDataBase(batchPoints);
+        Query query = new Query("SELECT * FROM cpu", dbName);
+        QueryResult queryResult = influxDBService.sendQuery(query);
+        return ResponseEntity.ok(Arrays.asList(queryResult.getResults()));
     }
 }
